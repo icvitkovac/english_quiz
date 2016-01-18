dd6707bad361783abd9d2eaae50df92e3cc835dc
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-01-18 17:14:42 +0100

    initial

---
+++ api/controllers/WordController.js
@@ -0,0 +1,32 @@
+/**
+ * WordController
+ *
+ * @description :: Server-side logic for managing words
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  create: function (req, res) {
+    console.log(req.body);
+    Word.create({value: req.body.word}).exec(function (err, data) {
+
+      var translations = [];
+
+      req.body.translations[parseInt(req.body.isAnswer)].isAnswer = true;
+
+      for (var i = 0; i < req.body.translations.length; i++) {
+        req.body.translations[i].term = data.id;
+      }
+
+      Translation.create(req.body.translations).exec(function (err, data) {
+
+        res.ok(data);
+      });
+
+    });
+
+  }
+
+};
+
---
+++ api/models/Translation.js
@@ -0,0 +1,31 @@
+/**
+ * Translation.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    term: {
+      model: 'word'
+    },
+    value: {
+      type: 'string',
+      required: true
+    },
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      autoincrement: true
+    },
+    isAnswer: {
+      type: 'boolean',
+      defaultsTo: 'false'
+    }
+
+  }
+};
+
