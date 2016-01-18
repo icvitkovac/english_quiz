4213091459e911fe5b07d9d58a42491d895d49b1
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-01-18 17:14:42 +0100

    initial

---
+++ api/models/Translation.js
@@ -0,0 +1,31 @@
+/**
+ * Translation.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    term: {
+      model: 'word'
+***REMOVED***,
+    value: {
+      type: 'string',
+      required: true
+***REMOVED***,
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      autoincrement: true
+***REMOVED***,
+    isAnswer: {
+      type: 'boolean',
+      defaultsTo: 'false'
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
---
+++ api/models/Word.js
@@ -0,0 +1,31 @@
+/**
+ * Word.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    value: {
+      type: 'string',
+      required: true
+***REMOVED***,
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      autoIncrement: true
+***REMOVED***,
+    translations: {
+      collection: 'translation',
+      via: 'term'
+***REMOVED***/*,
+    correctTranslation: {
+      model: 'answer'
+***REMOVED****/
+
+***REMOVED***
+***REMOVED***
+
