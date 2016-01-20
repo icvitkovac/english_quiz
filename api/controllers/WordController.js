98de8f6f69b6cf5436cbd74a887fba3b1538bb71
icvitkovac <cvitko87@gmail.com>
2016-01-20 21:39:12 +0100

    Added possibility to ask only hard words, improved word adding

--- api/controllers/WordController.js
+++ api/controllers/WordController.js
@@ -18,6 +18,8 @@ module.exports = {
         res.badRequest();
       }
 
+      var wordData = data;
+
       for (var i = 0; i < reqObj.translations.length; i++) {
         var tmpObj = {};
         tmpObj.term = data.id;
@@ -30,8 +32,11 @@ module.exports = {
       }
 
       Translation.create(translations).exec(function (err, data) {
-        //return res.ok(data, 'admin');
-        return res.ok();
+
+        if (err) {
+          res.badRequest();
+        }
+        return res.ok(wordData.value);
       });
 
     });
@@ -39,9 +44,23 @@ module.exports = {
   },
 
   find: function (req, res) {
-    Word.find().populate('translations').exec(function (err, data) {
+
+    Word.find(req.query).populate('translations').exec(function (err, data) {
+      if (err) res.badRequest(err);
       return res.ok(data);
     });
+  },
+
+  update: function (req, res) {
+    var reqObj = req.body;
+    var id = parseInt(reqObj.id);
+    delete reqObj.id;
+
+    Word.update(id, reqObj)
+      .exec(function (err, data) {
+        if (err) res.badRequest(err);
+        return res.ok(data);
+      });
   }
 
 };
--- api/models/Settings.js
+++ api/models/Settings.js
@@ -21,6 +21,11 @@ module.exports = {
     practiceMode: {
       type: 'boolean',
       defaultsTo: true
+    },
+    only_hard: {
+      type: 'boolean',
+      defaultsTo: false,
+      required: false
     }
 
   }
