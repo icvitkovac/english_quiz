1ae08cb5c94bbfdbaf6f7278d0a79e13fb5ff276
icvitkovac <cvitko87@gmail.com>
2016-04-17 19:30:11 +0200

    Added angular 2 prototype for getting all the words and displaying them

--- api/controllers/WordController.js
+++ api/controllers/WordController.js
@@ -2,7 +2,7 @@
  * WordController
  *
  * @description :: Server-side logic for managing words
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+
  */
 
 module.exports = {
