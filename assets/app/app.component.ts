74b21c233737e37e37b17ba0d88e53416914c8d0
icvitkovac <cvitko87@gmail.com>
2016-04-17 13:21:02 +0200

    Added angular 2 bootstrap

---
+++ assets/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent {
+}
+
+import {Component} from 'angular2/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent {
+}
