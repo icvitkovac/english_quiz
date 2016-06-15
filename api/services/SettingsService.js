5c1e325e6f4449e3b39b166bdf132ae5f24e8d65
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-06-15 17:05:58 +0200

    SettingsController refactor -> settings service

---
+++ api/services/SettingsService.js
@@ -0,0 +1,29 @@
+"use strict";
+module.exports = {
+
+
+  update: function (userId, reqObject, errorCb, successCb) {
+
+    Settings.update({userId: userId}, reqObject)
+      .exec(function (err, data) {
+        if (err) errorCb(err);
+        successCb(data);
+      });
+
+
+  },
+  init: function (userId, errorCb, successCb) {
+
+    Settings.findOrCreate({userId: userId}, {
+      userId: userId,
+      questionsNumber: 5,
+      practiceMode: true,
+      onlyHard: false
+    })
+      .exec(function (err, data) {
+        if (err) errorCb(err);
+        successCb(data);
+      });
+
+  }
+};
