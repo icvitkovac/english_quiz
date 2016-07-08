fc70763634c5340d01787762c07cf80c9c455185
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-08 11:11:20 +0200

    Updated Angular to rc4

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,12 +2,14 @@ import {Component} from '@angular/core';
 
 import {AdminComponent} from './admin/admin.component';
 import {MainComponent} from './main/main.component';
+import {StatsComponent} from './stats/stats.component';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
 
 @Component({
   selector: 'english-quiz',
   directives: [MainComponent, AdminComponent, ROUTER_DIRECTIVES],
+  precompile: [MainComponent, AdminComponent, StatsComponent],
   templateUrl: 'app/app.component.html'
 
 })
