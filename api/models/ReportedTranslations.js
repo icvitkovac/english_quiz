ffb0098ae486e23510b055fdbc4be63282a19718
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-23 17:40:02 +0200

    - refactored reportedTranslation model

---
+++ api/models/ReportedTranslations.js
@@ -0,0 +1,20 @@
+/**
+ * ReportedTranslations.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    reportedTranslation: {
+      model: 'translation'
+***REMOVED***,
+    reporter: {
+      model: 'user'
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
