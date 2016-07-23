393d9e23e2fd7da8f8b8d3fb0bdf5813f62c1368
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-23 16:03:39 +0200

    - project grooming

--- d61jodv2b78lu8_public_user.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public."user" (name, id, "facebookId", "createdAt", "updatedAt") VALUES ('Igor Cvitkovac', 18, '10154276477164015', '2016-06-03 10:39:05.000000', '2016-06-03 10:39:05.000000');
-INSERT INTO public."user" (name, id, "facebookId", "createdAt", "updatedAt") VALUES ('Drugačija Rešenja', 19, '1003567766404998', '2016-06-07 11:25:54.000000', '2016-06-07 11:25:54.000000');
-INSERT INTO public."user" (name, id, "facebookId", "createdAt", "updatedAt") VALUES ('Zgrabi Popust', 20, '837270119712764', '2016-06-10 23:51:38.000000', '2016-06-10 23:51:38.000000');
-INSERT INTO public."user" (name, id, "facebookId", "createdAt", "updatedAt") VALUES ('Karen Alabeabbeeabb Sharpeson', 21, '103978790029196', '2016-06-13 16:14:24.000000', '2016-06-13 16:14:24.000000');
