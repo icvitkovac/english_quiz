cb72ca8d03e8a1e4c2435d426def8784829b4e0a
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-26 22:09:48 +0200

    - added notification component

---
+++ assets/app/notification/notification.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { NotificationService } from './notification.service';
+import { Notification } from './notification.model';
+
+@Component({
+    selector: 'notification',
+     templateUrl: 'app/notification/notification.component.html'
+})
+export class NotificationComponent {
+    private _notes: Notification[];
+
+    constructor(private _notifications: NotificationService) {
+        this._notes = new Array<Notification>();
+
+        _notifications.noteAdded.subscribe(note => {
+            this._notes.push(note);
+
+            setTimeout(() => { this.hide.bind(this)(note) }, 3000000);
+    ***REMOVED***);
+***REMOVED***
+
+    private hide(note) {
+        let index = this._notes.indexOf(note);
+
+        if (index >= 0) {
+            this._notes.splice(index, 1);
+    ***REMOVED***
+***REMOVED***
+}
---
+++ assets/app/notification/notification.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Notification } from './notification.model';
+
+@Injectable()
+export class NotificationService {
+    private _notifications = new Subject<Notification>();
+
+    public noteAdded = this._notifications.asObservable();
+
+    public add(notification: Notification) {
+        this._notifications.next(notification);
+***REMOVED***
+}
