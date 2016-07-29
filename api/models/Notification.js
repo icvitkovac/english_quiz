1a9b9f6e2e548c2275cb0c418540b50994720ee0
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-29 18:04:44 +0200

    - added notification for reported word

--- api/models/Notification.js
+++ api/models/Notification.js
@@ -19,8 +19,7 @@ module.exports = {
     },
     code: {
       type: 'string',
-      // enum: Object.keys(notificationCodes)
-      enum: ['changeLanguage', 'reportedWord']
+      enum: ['reportedWord', 'generalNotification', 'comment']
     }
 
   }
