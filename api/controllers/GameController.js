15d530e273f6106bf07a90f21919eb77883bdfe0
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-29 23:01:59 +0200

    - words filtered by locale, which is also set when word is added

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -15,7 +15,7 @@
  * @param {object} gameData - passed Game data
  */
 function _findRandomWord(req, res, gameData) {
-  let authorQuery = {***REMOVED***
+  let authorQuery = {languageCode: req.session.user.locale***REMOVED***
   if (req.session.settings.practiceMode) {
     authorQuery = {author: req.session.user.id***REMOVED***
     if (req.session.game.askedWordsCount === req.session.settings.questionsNumber) {
