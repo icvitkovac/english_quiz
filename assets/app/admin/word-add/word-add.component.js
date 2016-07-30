85f55895da3c8e6844e2058d642ad0f2a8a22eab
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2016-07-30 19:20:21 +0200

    - moved word add component to admin folder

--- assets/app/admin/word-add/word-add.component.js
+++ assets/app/admin/word-add/word-add.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '../services/word.service', '../models/word', '../notification/notification.service', '../notification/notification.component'], function(exports_1, context_1) {
+System.register(['@angular/core', '../../services/word.service', '../../models/word', '../../notification/notification.service', '../../notification/notification.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
