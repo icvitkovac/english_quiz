adad27abb4e11594ab7b60f91622d668a2e15c63
icvitkovac <cvitko87@gmail.com>
2017-11-06 23:10:26 +0100

    Upgraded to ng 4

---
+++ assets/app/admin/word-update/word-update.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WordUpdateComponent } from './word-update.component';
+
+describe('WordUpdateComponent', () => {
+  let component: WordUpdateComponent;
+  let fixture: ComponentFixture<WordUpdateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WordUpdateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WordUpdateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
