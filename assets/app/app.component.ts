444a74c1d2f7b4f94ee614d458cbd732d7b46032
Igor Cvitkovac <igor.cvitkovac@sbgenomics.com>
2017-11-09 10:23:56 +0100

    angular production mode enabled

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private name: string;
+  public name: string;
 
   constructor() {
     try {
